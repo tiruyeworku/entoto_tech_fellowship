d96d7f5c6dedc1d55d855e38394b076c5d6bc0e1
Tsiyan <tsia.getachew@gmail.com>
2019-06-06 16:45:08 +0300

    All my file

---
+++ week1/day2/day2.js
@@ -0,0 +1,25 @@
+// var wetherTempratuer = 18
+// // console.log(wetherTempratuer)
+// if (wetherTempratuer <10)
+//   {
+//     console.log("It's cold outside")
+    
+//   }else if (wetherTempratuer > 20){
+//     console.log("It is warm outside")
+//   }else
+//     {
+//       console.log("It is nicec")
+//     }
+funciton(wetherTempratuer){
+  if (wetherTempratuer <10)
+  {
+    return("It's cold outside")
+    
+  }else if (wetherTempratuer > 20){
+    return("It is warm outside")
+  }else
+    {
+      return("It is nicec")
+    }
+  }
+console.log(wetherTempratuer(9))
